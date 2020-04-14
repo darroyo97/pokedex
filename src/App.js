809ff88a2d17666db00d961fecf4b52b7c0a8a4d
Daniela <daniela.arroyo27@gmail.com>
2020-04-13 20:33:46 -0500

    start set up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class App extends Component {
+    render() {
+        return (
+            <div>
+                This is App
+            </div>
+        )
+    }
+}
+
+export default App
